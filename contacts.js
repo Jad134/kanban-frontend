2f2711cf84f8c4037ac8591f1b69f31b5be7ce20
ShiningStoneSmasher <klarlhauertstein@gmail.com>
2023-09-16 14:17:55 +0200

    contact overlay add button and welcome

--- contacts.js
+++ contacts.js
@@ -3,11 +3,15 @@ function openContacts() {
         <div id="contact-overview">
             <div id="add-new-contact">
                 <p>Add new contact</p>
-                <img src="" alt="">
+                <img src="./assets/img/person_add.png" alt="">
             </div>
             <div>hier werden die kontakte hineingerendert</div>
         </div>
-        <div id="welcome-to-contacts"></div>
+        <div id="welcome-to-contacts">
+            <h1>Contacts</h1>
+            <div id="contact-seperator"></div>
+            <div iod="contact-slogan">Better with a team</div>
+        </div>
     
     `;
 }
