254f7874ba5cad33199c2c589c6553e4bdbfb130
Jad El Nader <Jadelnader99@gmail.com>
2023-09-18 23:00:31 +0200

    Button colors

--- assets/js/addTask.js
+++ assets/js/addTask.js
@@ -1,29 +1,12 @@
 function ChangeButtonColor(buttonId) {
     let button = document.getElementById(buttonId);
-    let urgentBtn = document.getElementById('urgent-btn');
-    let mediumBtn = document.getElementById('medium-btn')
-    let lowBtn = document.getElementById('low-btn');
-
-    // Überprüfen, ob die Button-Klasse 'active' hat
-    if (button.classList.contains('active')) {
-        // Wenn der Button die Klasse 'active' hat, entfernen Sie sie, um die Farbe zurückzusetzen
-        button.classList.remove('active');
-        
-    } else {
-        // Andernfalls fügen Sie die Klasse 'active' hinzu, um die Farbe zu ändern
-        button.classList.add('active');
-        
-    }
-
-    if (urgentBtn.classList.contains('active')){
-        mediumBtn.classList.remove('active');
-        lowBtn.classList.remove('active')
-    }
-    if (mediumBtn.classList.contains('active')){
-        urgentBtn.classList.remove('active');
-        lowBtn.classList.remove('active')
-    }
-
    
+    let buttons = document.querySelectorAll('.prio-buttons button');
+    buttons.forEach(function(btn) {
+        btn.classList.remove('active');
+    });
 
+    
+    // Fügen Sie die "active" Klasse nur zum ausgewählten Button hinzu
+    button.classList.add('active');
 }
