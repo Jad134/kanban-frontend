97202fade6423936e0dd240bf9aec734f1a5dde1
Heluedem <luedem.h@gmail.com>
2023-09-18 16:00:43 +0200

    styled index.html + inserted animation

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// blabla
+function init(){
+    const main = document.querySelector('.main-container');
+    main.style.opacity = "0";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const logo = document.getElementById('logo');
+    const main = document.querySelector('.main-container')
+    
+    setTimeout(() => {
+        logo.style.position = "absolute";
+        logo.style.top = "130px";
+        logo.style.left = "130px";
+        logo.style.transform = "translate(-50%, -50%) scale(0.5)";
+        logo.style.transition = "0.7s ease-out";
+
+        main.style.transition = "opacity 0.7s ease-in";
+        main.style.opacity = "1";
+    }, 500);
+});
