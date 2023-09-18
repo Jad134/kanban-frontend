ee23487cc501de89d1faccac076a1e66600457e1
ShiningStoneSmasher <klarlhauertstein@gmail.com>
2023-09-18 15:57:13 +0200

    array localstorage for contacts

--- contacts.js
+++ contacts.js
@@ -1,3 +1,5 @@
+let contacts = [];
+
 function openContacts() {
     document.getElementById('contacts').innerHTML = /*html*/ `
         <div id="contact-overview">
@@ -19,20 +21,14 @@ function openContacts() {
     `;
 }
 
-const CONTACTS = [];
-
-function renderContactsInOverview(){
-    let contactsInOverview = document.getElementById('render-contacts-ovewrview');
-    //contactsInOverview.innerHTML = ;
-}
 function addContact() {
     document.getElementById('detail-view-of-contacts').innerHTML = /*html */ `
         <div id="contact-name" class="contact-input-area">
-            <input type="text" placeholder="Name" required id="contact-name-input">
+            <input required type="text" placeholder="Name" id="contact-name-input">
             <img src="./assets/img/person.png" alt="Name" onclick="pushContactInfo()">
         </div>
         <div id="contact-email" class="contact-input-area">
-            <input type="email" placeholder="E-Mail" id="contact-email-input">
+            <input required type="email" placeholder="E-Mail" id="contact-email-input">
             <img src="./assets/img/mail.png" alt="E-Mail" onclick="pushContactInfo()">
         </div>
         <div id="contact-phone" class="contact-input-area">
@@ -52,13 +48,38 @@ function pushContactInfo(){
         'phone': phone
     };
 
-    CONTACTS.push(infos);
+    contacts.push(infos);
     console.log(infos);
 
-    let CONTACTSasString = JSON.stringify(CONTACTS);
-    localStorage.setItem('contacts', CONTACTSasString);
+    let contactsAsString = JSON.stringify(contacts);
+    localStorage.setItem('contacts', contactsAsString);
+
+    document.getElementById('contact-name-input').value = ``;
+    document.getElementById('contact-email-input').value = ``;
+    document.getElementById('contact-phone-input').value = ``;
+
 }
 function loadContacts(){
-    let CONTACTSasString = localStorage.getItem('contacts');
-    CONTACTS = JSON.parse(CONTACTSasString);
+    let contactsAsString = localStorage.getItem('contacts');
+    contacts = JSON.parse(contactsAsString);
+    console.log(contacts);
+    if (contacts !== null) {
+    renderContactsInOverview(contacts);
+    }
+}
+function renderContactsInOverview(contacts){
+    let contactsInOverview = document.getElementById('render-contacts-ovewrview');
+        
+    for (let i = 0; i < contacts.length; i++) {
+        let name = contacts[i]['name'];
+        let email = contacts[i]['email'];
+        let phone = contacts[i]['phone'];
+        
+        contactsInOverview.innerHTML += /*html*/ `
+        <div>${name}</div>
+        <div>${email}</div>
+        <div>${phone}</div>
+        `;
+    }
+
 }
