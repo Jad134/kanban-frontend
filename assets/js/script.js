574603148a575edb292db6ace43d95505e376456
Heluedem <luedem.h@gmail.com>
2023-09-19 12:47:01 +0200

    index.html changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 function init(){
+    renderContent();
     const main = document.querySelector('.main-container');
     main.style.opacity = "0";
 }
@@ -18,3 +19,63 @@ document.addEventListener("DOMContentLoaded", function () {
         main.style.opacity = "1";
     }, 500);
 });
+
+
+function renderContent() {
+    const middleContent = document.getElementById('middle-area');
+    middleContent.innerHTML+= `
+    <div class="border-radius-30 login">
+    <h1 class="padding-top">Log in</h1>
+    <div class="underline border-radius-8"></div>
+    <form onsubmit="" method="post">
+        <input class="login-input bg-email-icon icon" type="text" placeholder="Email" required/>
+        <input class="login-input bg-password-icon icon" type="password" placeholder="Password" required/>
+      <div class="checkbox-container">
+        <label class="checkbox-label">
+          <input name="checkbox" type="checkbox" />Remember me
+        </label>
+        <a class="startpage-links" href="#">I forgot my password</a>
+      </div>
+      <div class="login-buttons">
+        <button onclick="login()" id="login" class="h-button border-radius-8">Log in</button>
+        <button onclick="" id="guest-login" class="h-button-white border-radius-8">Guest Log in</button>
+      </div>
+    </form>
+    `;
+}
+
+function renderSignUp() {
+    const middleContent = document.getElementById('middle-area');
+    middleContent.innerHTML='';
+    middleContent.innerHTML+= `
+            <div class="border-radius-30 login">
+                <a href="index.html">
+                    <img src="assets/img/arrow-left.svg" class="arrow-left" alt="left arrow">
+                </a>
+              <h1>Sign up</h1>
+              <div class="underline border-radius-8"></div>
+              <form onsubmit="" method="post">
+                
+                <input class="login-input bg-password-icon icon" type="text" placeholder="Name" required/>
+                <input  class="login-input bg-email-icon icon" type="text" placeholder="Email" required/>
+                <input class="login-input bg-password-icon icon" type="password" placeholder="Password" required/>
+                <input class="login-input bg-password-icon icon" type="password" placeholder="Confirm Password" required/>
+    
+                <div class="checkbox-container-accept">
+                  <label class="checkbox-label">
+                    <input name="checkbox" type="checkbox" />I accept the<a class="startpage-links" href="#">Privacy Policy</a>
+                  </label>
+                </div>
+
+                <div class="login-buttons">
+                  <button  class="h-button border-radius-8">Sign up</button>
+                </div>
+              </form>
+            </div>
+          </div>
+    `;
+}
+
+function login() {
+    const login = document.getElementById('login');
+}
