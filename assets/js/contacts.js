efa939e1376c1419d5f4be1a58bc7cb6d3802591
ShiningStoneSmasher <klarlhauertstein@gmail.com>
2023-09-19 13:55:06 +0200

    storage for contacts

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -16,8 +16,7 @@ function openContacts() {
                 <div iod="contact-slogan">Better with a team</div>
             </div>
             <div id="detail-view-of-contacts">detailansicht ausgewählter kontakte</div>
-        </div>
-    
+        </div>    
     `;
 }
 
@@ -36,50 +35,4 @@ function addContact() {
             <img src="./assets/img/call.png" alt="Phone" onclick="pushContactInfo()">
         </div>
     `;
-}
-function pushContactInfo(){
-    let name = document.getElementById('contact-name-input').value;
-    let email = document.getElementById('contact-email-input').value;
-    let phone = document.getElementById('contact-phone-input').value;
-
-    let infos = {
-        'name': name,
-        'email': email,
-        'phone': phone
-    };
-
-    contacts.push(infos);
-    console.log(infos);
-
-    let contactsAsString = JSON.stringify(contacts);
-    localStorage.setItem('contacts', contactsAsString);
-
-    document.getElementById('contact-name-input').value = ``;
-    document.getElementById('contact-email-input').value = ``;
-    document.getElementById('contact-phone-input').value = ``;
-
-}
-function loadContacts(){
-    let contactsAsString = localStorage.getItem('contacts');
-    contacts = JSON.parse(contactsAsString);
-    console.log(contacts);
-    if (contacts !== null) {
-    renderContactsInOverview(contacts);
-    }
-}
-function renderContactsInOverview(contacts){
-    let contactsInOverview = document.getElementById('render-contacts-ovewrview');
-        
-    for (let i = 0; i < contacts.length; i++) {
-        let name = contacts[i]['name'];
-        let email = contacts[i]['email'];
-        let phone = contacts[i]['phone'];
-        
-        contactsInOverview.innerHTML += /*html*/ `
-        <div>${name}</div>
-        <div>${email}</div>
-        <div>${phone}</div>
-        `;
-    }
-
 }
---
+++ assets/js/storage.js
@@ -0,0 +1,14 @@
+
+
+const STORAGE_TOKEN = 'I6UE37M81WPHG1CYOP17O5XNIFP9VCIPG0GVDZE8';
+const URL = 'https://remote-storage.developerakademie.org/item';
+
+async function setContact(key, value){
+    const payload = {key, value, token: STORAGE_TOKEN}
+    return fetch(URL, { method: 'POST', body: JSON.stringify(payload)}).then(res => res.json());
+}
+
+async function getContact(key){
+    const url =`${URL}?key=${key}&token=${STORAGE_TOKEN}`;
+    return fetch(url).then(res => res.json());
+}
