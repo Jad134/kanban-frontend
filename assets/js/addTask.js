dd6ce1dd7b383814fbaaec89ae31e09d57868fa0
Jad El Nader <Jadelnader99@gmail.com>
2023-09-19 20:18:02 +0200

    finetunung

--- assets/js/addTask.js
+++ assets/js/addTask.js
@@ -1,12 +1,23 @@
-function ChangeButtonColor(buttonId) {
+function ChangeButtonColor(buttonId, imgId) {
     let button = document.getElementById(buttonId);
-   
+    let img = document.getElementById(imgId)
+
     let buttons = document.querySelectorAll('.prio-buttons button');
-    buttons.forEach(function(btn) {
+    buttons.forEach(function (btn,) {
         btn.classList.remove('active');
+        
     });
 
-    
+    let images = document.querySelectorAll('.prio-buttons button img');
+    images.forEach(function (imag,) {
+        imag.classList.remove('active');
+        
+    });
+
+
     // Fügen Sie die "active" Klasse nur zum ausgewählten Button hinzu
     button.classList.add('active');
+    img.classList.add('active')
+
+
 }
