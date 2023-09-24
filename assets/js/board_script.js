8f4034dfdff7389f135a8ace65ae5150430d610f
Alexander Riedel <alexander.riedel@riedelmedia.de>
2023-09-24 22:14:21 +0200

    add sample data for board

--- assets/js/board_script.js
+++ assets/js/board_script.js
@@ -1,5 +1,54 @@
-
-
 /*function openBoard() {
     document.getElementById('board').innerHTML = '';
 }*/
+
+
+let testTasks = [{
+    "id": 0,
+    "title": "Kochwelt Page & Recipe Recommender",
+    "description": "Build start page with recipe recommendation.",
+    "assigned": ["Emanuel Mauer", "Marcel Bauer", "Anton Mayer"],
+    "date": "20230510",
+    "prio": "medium",
+    "category": "User Story",
+    "subtask": [
+        {
+            "id": 0,
+            "title": "Implement Recipe Recommendation",
+            "done": true
+        },
+        {
+            "id": 1,
+            "title": "Start Page Layout",
+            "done": false
+        }
+    ]
+},
+{
+    "id": 1,
+    "title": "CSS Architecture Planning",
+    "description": "Define CSS naming conventions and structure.",
+    "assigned": ["Sofia Müller (You)", "Benedikt Ziegler"],
+    "date": "20230902",
+    "prio": "urgent",
+    "category": "Technical Task",
+    "subtask": [
+        {
+            "id": 0,
+            "title": "Establish CSS Methodology",
+            "done": true
+        },
+        {
+            "id": 1,
+            "title": "Setup Base Styles",
+            "done": true
+        },
+        {
+            "id": 2,
+            "title": "Subtaks 3",
+            "done": true
+        }
+    ]
+}];
+
+
