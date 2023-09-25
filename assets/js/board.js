1919a264d7c0bc042fd7cc3a00819b51a693801c
Alexander Riedel <alexander.riedel@riedelmedia.de>
2023-09-25 09:04:43 +0200

    add testdata and javascript logic

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,27 +1,96 @@
-/*function openBoard() {
-    document.getElementById('board').innerHTML = '';
-}*/
+let testTasks = [{}];
 
 
+async function loadTestTasks() {
+    testTasks = [];
+    let testTask = await getItem('test');
+    testTask = JSON.parse(testTask['data']['value']);
+
+    for (let i = 0; i < testTask.length; i++) {
+        let loadedTask = testTask[i];
+        testTasks.push(loadedTask);
+    }
+
+    renderTestTasks();
+}
+
+
+function renderTestTasks() {
+    for (let i = 0; i < testTasks.length; i++) {
+        let id = testTasks[i]['id'];
+        console.log(id);
+
+        let bucket = testTasks[i]['bucket'];
+        console.log(bucket);
+
+        let title = testTasks[i]['title'];
+        console.log(title);
+
+        let description = testTasks[i]['description'];
+        console.log(description);
+
+        let assigned = testTasks[i]['assigned'];
+        console.log(assigned);
+
+        let duedate = testTasks[i]['duedate'];
+        console.log(duedate);
+
+        let prio = testTasks[i]['prio'];
+        console.log(prio);
+
+        let category = testTasks[i]['category'];
+        console.log(category);
+
+        let subtask = testTasks[i]['subtask'];
+        console.log(subtask);
+    }
+}
+
+
+async function addTestTask() {
+    //let taskCounter;
+    let addTestTask = {
+        "id": 2,            // sollte durch einen Zähler ermittelt werden
+        "bucket": "done",
+        "title": "HTML Base Template Creation",
+        "description": "Create reusable HTML base templates...",
+        "assigned": ["Benedikt Ziegler"],
+        "duedate": "20231007",
+        "prio": "low",
+        "category": "Technical Task",
+        "subtask": []
+    };
+    testTasks.push(addTestTask);
+}
+
+
+async function saveTestTasks() {
+    await setItem('test', JSON.stringify(testTasks));
+}
+
+
+// TESTDATA
+
+/*
 let testTasks = [{
     "id": 0,
     "bucket": "in-progress",
     "title": "Kochwelt Page & Recipe Recommender",
     "description": "Build start page with recipe recommendation.",
     "assigned": ["Emanuel Mauer", "Marcel Bauer", "Anton Mayer"],
-    "date": "20230510",
+    "duedate": "20230510",
     "prio": "medium",
     "category": "User Story",
     "subtask": [
         {
-            "id": 0,
-            "title": "Implement Recipe Recommendation",
-            "done": true
+            "subid": 0,
+            "subtitle": "Implement Recipe Recommendation",
+            "subdone": true
         },
         {
-            "id": 1,
-            "title": "Start Page Layout",
-            "done": false
+            "subid": 1,
+            "subtitle": "Start Page Layout",
+            "subdone": false
         }
     ]
 },
@@ -31,53 +100,25 @@ let testTasks = [{
     "title": "CSS Architecture Planning",
     "description": "Define CSS naming conventions and structure.",
     "assigned": ["Sofia Müller (You)", "Benedikt Ziegler"],
-    "date": "20230902",
+    "duedate": "20230902",
     "prio": "urgent",
     "category": "Technical Task",
     "subtask": [
         {
-            "id": 0,
+            "subid": 0,
             "title": "Establish CSS Methodology",
             "done": true
         },
         {
-            "id": 1,
+            "subid": 1,
             "title": "Setup Base Styles",
             "done": true
         },
         {
-            "id": 2,
-            "title": "Subtaks 3",
-            "done": true
+            "subid": 2,
+            "subtitle": "Subtaks 3",
+            "subdone": false
         }
     ]
 }];
-
-
-function loadTestTasks() {
-    //document.getElementById('todo').innerHTML = '';
-    if ( testTasks[1] ) {
-        console.log('true');
-    } else {
-        console.log('false');
-    }
-
-    //loadTestData();
-}
-
-
-async function saveTestTasks() {
-    await setItem('account', JSON.stringify(accounts));
-}
-
-
-/*
-async function loadTestData() {
-    testArray = [];
-    try {
-        testArray = JSON.parse(await getItem('tasks'));
-    } catch (error) {
-        console.error('Loading error:', error);
-    }
-}
 */
