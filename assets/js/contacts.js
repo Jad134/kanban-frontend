e8529ff7bda60ef83e94f8be0bb74ffc44e9eeb1
ShiningStoneSmasher <klarlhauertstein@gmail.com>
2023-09-27 12:14:37 +0200

    cardslider finish

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -76,10 +76,10 @@ async function slideInCard(){
     startAnimation();
 }
 async function waitForIt(){
-    let background = document.getElementById('color-my-back');       
+    let slider = document.getElementById('slide-container');       
     let deactivateOverflow = document.body;
     
-    background.style.display = "flex";
+    slider.style.display = "flex";
     deactivateOverflow.classList.add('hide-my-scrolls');
 }
 async function startAnimation(){   
@@ -89,10 +89,15 @@ async function startAnimation(){
 }
 function closeContactAddCard(){
     let animation = document.getElementById('add-contact-card');
-    let background = document.getElementById('color-my-back');
+    let slider = document.getElementById('slide-container');
+    let back = document.getElementById('color-my-back');
         
     animation.style.right = "-200%";
-    background.style.display = "none";
+    slider.style.display = "none";
+    setTimeout(() => {
+        back.style.backgroundColor = 'rgba(0, 0, 0, 0.0)';
+      }, 100);
+    back.style.display = "none"
         
     animation.addEventListener('transitionend', function() {
         if (animation.style.right === "-200%") {
@@ -100,74 +105,25 @@ function closeContactAddCard(){
         let shrikDiv = document.getElementById('create-contact');
         activateOverflow.classList.remove('hide-my-scrolls');
         animation.style.width = "0%";
+        shrikDiv.style.width = "0%";
         shrikDiv.style.display = "none";
         }
         })
 }
-function renderAddContactCard() {
-    document.getElementById('add-contact-card').innerHTML = /*html */ `
-    <div id="color-my-back" onclick="closeContactAddCard()"></div>
-    <div id="create-contact" onclick="">
-
-        <div id="design-sector">
-            <img src="./assets/img/logo4SideBar.svg" alt="join logo">
-            <h2>Add contact</h2>
-            <p>Tasks are better with a team!</p>
-            <div id="horizon-seperator"></div>    
-        </div>
-
-        <div id="fill-in-sector">
-            <div id="move-img">
-                <div class="cancel-svg" onclick="closeContactAddCard()">
-                    <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path id="change-cancel-svg-color-head" d="M12.2496 11.9998L17.4926 17.2428M7.00659 17.2428L12.2496 11.9998L7.00659 17.2428ZM17.4926 6.75684L12.2486 11.9998L17.4926 6.75684ZM12.2486 
-                        11.9998L7.00659 6.75684L12.2486 11.9998Z" stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                </div>
-            </div>
-            <div id="img-and-inputs">
-                <div id="put-the-img">
-                    <img id="img-big-person" src="./assets/img/person.svg" alt="">
-                </div>
-                <div>
-                    <div id="input-fields">
-                        <div id="contact-name" class="contact-input-area">
-                            <input required type="text" placeholder="Name" id="contact-name-input">
-                            <img src="./assets/img/person.png" alt="Name" onclick="pushContactInfo()">
-                        </div>
-                        <div id="contact-email" class="contact-input-area">
-                            <input required type="email" placeholder="E-Mail" id="contact-email-input">
-                            <img src="./assets/img/mail.png" alt="E-Mail" onclick="pushContactInfo()">
-                        </div>
-                        <div id="contact-phone" class="contact-input-area">
-                            <input type="tel" placeholder="Phone" id="contact-phone-input">
-                            <img src="./assets/img/call.png" alt="Phone" onclick="pushContactInfo()">
-                        </div>
-                    </div>
-            
-                    <div id="cancel-and-create-contact-btns">
-                        <div id="cancel-contact-btn" onclick="closeContactAddCard()">
-                            <p>Cancel</p>
-                            <div class="cancel-create-img">
-                                <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path id="change-cancel-svg-color-btn" d="M12.2496 11.9998L17.4926 17.2428M7.00659 17.2428L12.2496 11.9998L7.00659 17.2428ZM17.4926 6.75684L12.2486 11.9998L17.4926 6.75684ZM12.2486 
-                                        11.9998L7.00659 6.75684L12.2486 11.9998Z" stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                </svg>
-                            </div>
-                        </div>
-                        <div id="create-contact-btn" onclick="pushContactInfo()">
-                            <p>Create Task</p>
-                            <img class="cancel-create-img" src="./assets/img/check.svg" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-        </div>
-
-    </div>
-
-    `;
+async function renderAddContactCard() {
+    await showMyCard();
+    await colorMyBack();
     slideInCard();
+}
+async function showMyCard(){
+    document.getElementById('add-contact-card').style.width = "100%";
+    document.getElementById('create-contact').style.width = "100%";
+    document.getElementById('create-contact').style.display = "flex";
+}
+async function colorMyBack(){
+        const background = document.getElementById('color-my-back');
+        background.style.display = 'block';
+        setTimeout(() => {
+          background.style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+        }, 100);
 }
