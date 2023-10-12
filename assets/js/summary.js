7573369682637c00394bf16aee6eacbdee12ad0a
Heluedem <luedem.h@gmail.com>
2023-10-12 20:09:44 +0200

    added nextDueDate/urgent to summary

--- assets/js/summary.js
+++ assets/js/summary.js
@@ -17,11 +17,11 @@ function renderGreeting() {
     } else {
         greeting = 'Good evening';
     }
-    
+
     if (greetingName === 'Guest') {
         document.getElementById('greeting-daytime').innerHTML = greeting;
         document.getElementById('greeting-name').innerHTML = '';
-    } 
+    }
     else {
         document.getElementById('greeting-daytime').innerHTML = greeting + ',';
         document.getElementById('greeting-name').innerHTML = greetingName;
@@ -29,30 +29,69 @@ function renderGreeting() {
 }
 
 // ----------------- Summary Calculation ---------------
-function totalTasks() {                                                       
+function totalTasks() {
     let total = addedTasks.length;
     let totalInBoard = document.getElementById('total-tasks');
     totalInBoard.textContent = total;
 }
-    getTaskStorage().then(() => {                                  
+getTaskStorage().then(() => {
     totalTasks();
     urgentTasks();
+    nextDueDate();
     updateSummary()
-
 });
 
 
-function urgentTasks() {    
-    let taskContainer = 0;                                                    
+function urgentTasks() {
+    let taskContainer = 0;
     for (let i = 0; i < addedTasks.length; i++) {
-        if (addedTasks[i].prio === 'Urgent') {                      
+        if (addedTasks[i].prio === 'Urgent') {
             taskContainer++;
         }
     }
-    let feedback = document.getElementById('urgent-tasks');                   
+    let feedback = document.getElementById('urgent-tasks');
     feedback.textContent = taskContainer;
 }
 
+
+/* function formatDate(date) {
+    let day = date.getDate();
+    let month = date.toLocaleString('default', { month: 'long' });
+    let year = date.getFullYear();
+    return `${month} ${day}, ${year}`;
+} */
+
+
+function calculateClosestDueDate(closestDueDate) {
+    let nextDate = document.getElementById('next-urgent-date');
+    if (closestDueDate !== null) {
+        let information = { year: 'numeric', month: 'long', day: 'numeric' };
+        let formattedDate = closestDueDate.toLocaleDateString('en-US', information); // sonst "undefined" für die Standardeinstl. nötig
+        nextDate.innerHTML = formattedDate;
+    }
+}
+
+
+function nextDueDate() {
+    let closestDueDate = null;
+   
+    let today = new Date();
+    if (!addedTasks) {                                      // Fehler abfangen
+        return;
+    } else if (addedTasks.length === 0) {                   
+        let message = document.getElementById('next-urgent-tasks');
+        message.innerHTML = 'No urgent tasks scheduled';
+    }
+    for (let i = 0; i < addedTasks.length; i++) {
+        let taskDueDate = new Date(addedTasks[i]['duedate']);
+        if (taskDueDate > today) {
+            if (closestDueDate === null || taskDueDate < closestDueDate) {
+                closestDueDate = taskDueDate;
+            }}
+            calculateClosestDueDate(closestDueDate);
+        }
+        }
+        
 
 function updateSummary() {
     let taskCounts = {
@@ -65,7 +104,8 @@ function updateSummary() {
         let bucket = addedTasks[i].bucket;
         if (taskCounts[bucket] !== undefined) {
             taskCounts[bucket]++;
-        }}
+        }
+    }
     document.getElementById('tasks-in-progress').textContent = taskCounts['in-progress'];
     document.getElementById('todos').textContent = taskCounts['todo'];
     document.getElementById('completed-tasks').textContent = taskCounts['done'];
