8a1aa86512019142a5274932250878bcf83fcd9a
Alexander Riedel <alexander.riedel@gus-group.com>
2023-10-31 11:58:51 +0100

    update timeout ms

--- assets/js/edit-task.js
+++ assets/js/edit-task.js
@@ -236,5 +236,5 @@ function sliderTaskEdited() {
     setTimeout(function () {
         closeSlider();
         document.getElementById('slider-container').innerHTML = '';
-    }, 1300);
+    }, 1500);
 }
