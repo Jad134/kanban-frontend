5b73912b8470ac8e2984e070a2ec3dfa1fee881c
Jad El Nader <Jadelnader99@gmail.com>
2023-11-08 08:14:24 +0100

    Clean Coding

---
+++ assets/js/addTask-assets.js
@@ -0,0 +1,200 @@
+/**
+ * Open the Contacts to add them to a task. The container is only invisible with display: none;
+ */
+function openContactOverlay() {
+    let onclick = document.getElementById('assignedTo');
+    let overlayContainer = document.getElementById('contact-overlay');
+    let requiredInfo = document.getElementById('required-info');
+
+    requiredInfo.classList.remove('d-flex');
+    requiredInfo.classList.add('d-none');
+    overlayContainer.classList.remove('d-none');
+    overlayContainer.classList.add('d-flex');
+    onclick.style.backgroundImage = "url(./assets/img/arrow-up.svg)";
+
+    onclick.removeAttribute('onClick');
+
+    document.addEventListener('click', closeOnClickOutside);
+    onclick.onclick = closeContactOverlay;
+}
+
+
+/**
+ * This function styles the edit Subtasks section and the Input fields
+ * 
+ * @param {number} i 
+ * @param {html-Element} currenTask 
+ * @param {html-Element} subtaskList 
+ * @param {html-Element} editSubInput 
+ * @param {html-Element} taskbtn 
+ * @param {html-Element} inputContainer 
+ */
+function styleEditSubTask(i, currenTask, subtaskList, editSubInput, taskbtn, inputContainer) {
+    inputContainer.classList.add('d-flex');
+    inputContainer.classList.remove('d-none');
+    taskbtn.classList.remove('d-flex');
+    taskbtn.classList.add('d-none');
+    subtaskList.classList.remove('subtask-list');
+    subtaskList.classList.add('d-none');
+    editSubInput.classList.remove('d-none');
+    editSubInput.value = `${currenTask}`;
+}
+
+/**
+ * This function is responsible for managing the buttons within the 'add subtask' input field and enables the buttons to appear or disappear.
+ */
+function handleInputFocus() {
+    const inputField = document.getElementById('subtask-input');
+    const svgContainer = document.querySelector('.subtask-svg');
+    const subtaskbtn = document.getElementById('subtask-button');
+
+    inputField.addEventListener('focus', () => {
+        svgContainer.style.display = 'flex';
+        subtaskbtn.style.display = 'none';
+    });
+
+    inputField.addEventListener('blur', () => {
+        svgContainer.style.display = 'none';
+        subtaskbtn.style.display = 'flex';
+    });
+}
+
+
+/**
+ * Clears values and content in various HTML elements.
+ *
+ * @param {HTMLElement} title - The HTML input element for the title.
+ * @param {HTMLElement} description - The HTML input element for the description.
+ * @param {HTMLElement} date - The HTML input element for the date.
+ * @param {Array<HTMLElement>} sublist - An array of HTML elements representing subtasks.
+ * @param {HTMLElement} newTasks - The HTML input element for new tasks.
+ * @param {HTMLElement} contactImg - The HTML element for the contact image.
+ */
+function removeValues(title, description, date, sublist, newTasks, contactImg) {
+    contactImg.innerHTML = "";
+    newTasks.value = "";
+    date.value = "";
+    description.value = "";
+    title.value = "";
+    sublist.forEach(function (element) {
+        element.innerHTML = '';
+    })
+}
+
+
+/**
+ * Removes the Checkbox Styling for the contacts, if the page was cleared.
+ */
+function removeCheckboxStyle() {
+    let overlayContainer = document.getElementById('contact-overlay');
+    overlayContainer.innerHTML = '';
+    loadContacts();
+}
+
+
+/**
+ * Handles the Enter key press event and performs specific actions based on the provided action type.
+ *
+ * @param {Event} event - The key press event object.
+ * @param {string} action - The type of action to perform ('subtask-input' or 'edit-Input').
+ * @param {number} i - The index parameter used for specific actions (e.g., renaming a subtask).
+ */
+function handleEnterKeyPress(event, action, i) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        if (action === 'subtask-input') {
+            addSubTask();
+        } else if (action === 'edit-Input') {
+            renameSubTask(i);
+        }
+    }
+}
+
+
+/**
+ * Search function to find Contacts in the List.
+ */
+function findContact() {
+    const searchInput = document.getElementById('assignedTo');
+    const contactCards = document.querySelectorAll('.add-task-contacts');
+    searchInput.addEventListener('input', () => {
+        const searchText = searchInput.value.toLowerCase();
+        contactCards.forEach((card) => {
+            const cardText = card.innerText.toLowerCase();
+            if (cardText.includes(searchText)) {
+                card.style.display = 'flex';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    });
+}
+
+
+/**
+ * Removes the Prio button Color.
+ */
+function removeButtonColor() {
+    let buttons = document.querySelectorAll('.prio-buttons button');
+    buttons.forEach(function (btn,) {
+        btn.classList.remove('active');
+    });
+    let images = document.querySelectorAll('.prio-buttons button img');
+    images.forEach(function (imag,) {
+        imag.classList.remove('active');
+    });
+}
+
+
+/**
+ * Closes a UI element, specified by `overlayContainer`, when a click event occurs outside the designated area.
+ *
+ * @param {MouseEvent} event - The click event object.
+ * @returns {void}
+ */
+function closeOnClickOutside(event) {
+    let overlayContainer = document.getElementById('contact-overlay');
+    let assignedTo = document.getElementById('assignedTo');
+
+    if (!overlayContainer.contains(event.target) && event.target !== assignedTo) {
+        closeContactOverlay();
+    }
+}
+
+
+/**
+ * Styling for the Contact list, which is only opend and closed by CSS Styling
+ */
+function closeContactOverlay() {
+    let overlayContainer = document.getElementById('contact-overlay');
+    let onclick = document.getElementById('assignedTo');
+    let requiredInfo = document.getElementById('required-info');
+
+    requiredInfo.classList.remove('d-none');
+    requiredInfo.classList.add('d-flex');
+    overlayContainer.classList.remove('d-flex');
+    overlayContainer.classList.add('d-none');
+    onclick.style.backgroundImage = "url(./assets/img/arrow-assign-down.svg)";
+
+    document.removeEventListener('click', closeOnClickOutside);
+    onclick.onclick = openContactOverlay;
+}
+
+
+/**
+ * removes everything from the page
+ */
+function clearTasks() {
+    let title = document.getElementById('title-input');
+    let description = document.getElementById('description-textarea');
+    let date = document.getElementById('date-input');
+    let sublist = document.querySelectorAll('.sublist-container');
+    let newTasks = document.getElementById('subtask-input');
+    let contactImg = document.getElementById('selected-contacts');
+
+    removeValues(title, description, date, sublist, newTasks, contactImg);
+    newSubTasks = [];
+    assignedContact = [];
+    removeCheckboxStyle();
+    removeButtonColor();
+}
--- assets/js/addTask.js
+++ assets/js/addTask.js
@@ -12,6 +12,7 @@ function init() {
     handleInputFocus();
 }
 
+
 /**
  * Change the Prio Button Color at onClick and activate only one button
  * 
@@ -34,6 +35,7 @@ function ChangeButtonColor(buttonId, imgId) {
     }
 }
 
+
 /**
  * Reset the color of the buttons with an extra class names .active */
 function resetButtons() {
@@ -47,6 +49,7 @@ function resetButtons() {
         imag.classList.remove('active');
     });
 }
+
 
 /**
  * Use the values from the page and push them to an JSON array. Then starts a funciton which push the array to an remote storage
@@ -73,6 +76,7 @@ function getValues() {
     sendFormular(tasks);
 }
 
+
 /**
  * Controls the from validation for true or false to upload content
  */
@@ -84,6 +88,8 @@ function submitForm() {
         return false; // Das Formular wird nicht abgesendet, wenn die Validierung fehlschlägt
     }
 }
+
+
 /**
  * Validates an input field and displays error messages if the field is empty.
  * 
@@ -106,6 +112,8 @@ function validateField(field) {
         return true;
     }
 }
+
+
 /**
  * Validates a form by checking each field in the list of fields to validate.
  * 
@@ -127,6 +135,11 @@ function validateForm() {
 }
 
 
+/**
+ * push the new task to the addedTasks Array and clear current Arrays.
+ * 
+ * @param {Array} tasks 
+ */
 async function sendFormular(tasks) {
     addedTasks.push(tasks);
     await addTaskToStorage();
@@ -148,6 +161,9 @@ async function sendFormular(tasks) {
 }
 
 
+/**
+ * Slider for the information, that the task was successfully added to the Board
+ */
 function sliderTaskAdded() {
     document.getElementById('slider-container').innerHTML = renderTaskAdded();
 
@@ -159,19 +175,9 @@ function sliderTaskAdded() {
 }
 
 
-function renderTaskAdded() {
-    return `
-        <div class="task-added-button">
-            <span>Task added to board</span>
-            <svg width="31" height="26" viewBox="0 0 31 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M23.4544 2.77273L23.4545 23.2271C23.4538 23.8296 23.2142 24.4074 22.7881 24.8334C22.362 25.2595 21.7843 25.4992 21.1817 25.4998L16.6363 25.4998C16.0338 25.4992 15.456 25.2595 15.03 24.8334C14.6039 24.4074 14.3642 23.8296 14.3636 23.2271L14.3636 2.77273C14.3642 2.17015 14.6039 1.59243 15.03 1.16635C15.456 0.740262 16.0338 0.500623 16.6363 0.50002L21.1817 0.50002C21.7843 0.500623 22.362 0.740262 22.7881 1.16635C23.2142 1.59243 23.4538 2.17015 23.4544 2.77273ZM16.6363 23.2271L21.1817 23.2271L21.1817 2.77273L16.6363 2.77273L16.6363 23.2271ZM16.6363 2.77273L16.6363 23.2271C16.6357 23.8296 16.3961 24.4073 15.97 24.8334C15.5439 25.2595 14.9662 25.4991 14.3636 25.4997L9.81823 25.4997C9.21566 25.4991 8.63794 25.2595 8.21185 24.8334C7.78577 24.4073 7.54613 23.8296 7.54553 23.227L7.54553 2.7727C7.54613 2.17013 7.78577 1.59241 8.21185 1.16632C8.63793 0.740238 9.21566 0.500602 9.81823 0.5L14.3636 0.499999C14.9662 0.500602 15.5439 0.740238 15.97 1.16632C16.3961 1.59241 16.6357 2.17015 16.6363 2.77273ZM9.81823 23.227L14.3636 23.2271L14.3636 2.77273L9.81823 2.7727L9.81823 23.227ZM9.81823 2.7727L9.81823 23.227C9.81763 23.8296 9.57799 24.4073 9.15191 24.8334C8.72582 25.2595 8.1481 25.4991 7.54553 25.4997L3.00012 25.4997C2.39755 25.4991 1.81983 25.2595 1.39374 24.8334C0.967657 24.4073 0.728019 23.8296 0.727417 23.227L0.727416 2.7727C0.728018 2.17013 0.967656 1.59241 1.39374 1.16632C1.81982 0.740238 2.39755 0.500603 3.00012 0.5L7.54553 0.5C8.1481 0.500602 8.72582 0.740238 9.1519 1.16632C9.57799 1.59241 9.81763 2.17013 9.81823 2.7727ZM3.00012 23.227L7.54553 23.227L7.54553 2.7727L3.00012 2.7727L3.00012 23.227Z" fill="white"/>
-                <path d="M30.2726 2.77298L30.2726 23.2273C30.272 23.8299 30.0323 24.4076 29.6062 24.8337C29.1802 25.2598 28.6024 25.4994 27.9999 25.5L23.4545 25.5C22.8519 25.4994 22.2742 25.2598 21.8481 24.8337C21.422 24.4076 21.1824 23.8296 21.1817 23.2271L21.1817 2.77273C21.1823 2.17015 21.422 1.59268 21.8481 1.1666C22.2742 0.740514 22.8519 0.500876 23.4544 0.500274L27.9999 0.500273C28.6024 0.500876 29.1801 0.740514 29.6062 1.1666C30.0323 1.59268 30.272 2.1704 30.2726 2.77298ZM23.4545 23.2271L27.9999 23.2273L27.9999 2.77298L23.4544 2.77273L23.4545 23.2271Z" fill="white"/>
-            </svg>
-        </div>
-    `
-}
-
-
+/**
+ * Returns the "todo" bucket for the Board.
+ */
 function getBucketFromSession() {
     bucket = sessionStorage.getItem('bucket');
     if (bucket === null || bucket === undefined) {
@@ -187,6 +193,14 @@ async function addTaskIdToStorage() {
 }
 
 
+async function addTaskToStorage() {
+    await setItem('tasks', JSON.stringify(addedTasks));
+}
+
+
+/**
+ * Load the user Datas from the Remote storage
+ */
 async function loadUserDataFromRemote() {
     let newUserDataString = await getItem('users');
     newUserDataString = JSON.parse(newUserDataString['data']['value']);
@@ -197,6 +211,7 @@ async function loadUserDataFromRemote() {
     loadContacts();
     findContact();
 }
+
 
 /**
  * Get the existing tasks.
@@ -213,11 +228,9 @@ async function getTaskStorage() {
 }
 
 
-async function addTaskToStorage() {
-    await setItem('tasks', JSON.stringify(addedTasks));
-}
-
-
+/**
+ * Add Subtask to the list.
+ */
 function addSubTask() {
     let subtaskContent = document.getElementById('subtask-lists');
     let newTasksText = document.getElementById('subtask-input').value;
@@ -236,66 +249,12 @@ function addSubTask() {
         subtaskContent.innerHTML += renderSubTask(newTasks, i);
     }
     document.getElementById('subtask-input').value = '';
-
 }
 
-
-function renderSubTask(newTasks, i) {
-    return /*html*/`
-    <div id="sublist-container${i}" class="sublist-container">
-      <ul id="subtask-list${i}" class="subtask-list">
-            <li> <span  id="show-current-subtask${i}">${newTasks}</span></li>
-      </ul>
-        <div id="subtask-input-container${i}" class="d-none subtask-input-container" style="width: 100%;"> 
-           <input onkeydown="handleEnterKeyPress(event, 'edit-Input', ${i})"  id="edit-task-input${i}" class=" edit-subtask-input" type="text" > 
-           <img onclick="renameSubTask(${i})" class="edit-done"  src="/assets/img/done.svg" alt="">
-           <img onclick="deleteSubTask(${i})"  src="/assets/img/addtasktrash.svg" alt="">
-        </div>
-        <div id="task-edit-buttons${i}" class="d-flex subtask-edit-buttons">
-          <img onclick="editSubTask(${i}, '${newTasks}')" class="d-none edit-subtask" src="/assets/img/addtaskedit.svg" alt="">
-          <img onclick="deleteSubTask(${i})" class="d-none" style="height: 24px; width: 24px;" src="/assets/img/addtasktrash.svg" alt="">
-        </div>
-    </div> `;
-
-}
 
 /**
- * Open the Contacts to add them to a task. The container is only invisible with display: none;
+ * Load the contacts from the userData Array
  */
-function openContactOverlay() {
-    let onclick = document.getElementById('assignedTo');
-    let overlayContainer = document.getElementById('contact-overlay');
-    let requiredInfo = document.getElementById('required-info');
-
-    requiredInfo.classList.remove('d-flex');
-    requiredInfo.classList.add('d-none');
-    overlayContainer.classList.remove('d-none');
-    overlayContainer.classList.add('d-flex');
-    onclick.style.backgroundImage = "url(./assets/img/arrow-up.svg)";
-
-    onclick.removeAttribute('onClick');
-
-    document.addEventListener('click', closeOnClickOutside);
-    onclick.onclick = closeContactOverlay;
-}
-
-
-function closeContactOverlay() {
-    let overlayContainer = document.getElementById('contact-overlay');
-    let onclick = document.getElementById('assignedTo');
-    let requiredInfo = document.getElementById('required-info');
-
-    requiredInfo.classList.remove('d-none');
-    requiredInfo.classList.add('d-flex');
-    overlayContainer.classList.remove('d-flex');
-    overlayContainer.classList.add('d-none');
-    onclick.style.backgroundImage = "url(./assets/img/arrow-assign-down.svg)";
-
-    document.removeEventListener('click', closeOnClickOutside);
-    onclick.onclick = openContactOverlay;
-}
-
-
 function loadContacts() {
     let overlayContainer = document.getElementById('contact-overlay');
     let loginUser = localStorage.getItem('login-name');
@@ -314,20 +273,6 @@ function loadContacts() {
     }
 }
 
-
-function renderContacts(name, i, userInitial, youLabel) {
-    return /*html*/`
-    <label class="contact-label" for="check-contact${i}">
-        <div class="current-contacts">
-            <div class="add-task-contacts"> 
-               <div id="list-circle${i}" class="contact-circle"> <span>${userInitial}</span></div>
-               <span class="current-name">${name} ${youLabel}</span>
-               <input value="${name}" class="check-contact" id="check-contact${i}" type="checkbox" onchange="setCheckbox(this, '${name}', ${i})">
-            </div>
-        </div>
-    </label>        
-    `;
-}
 
 /**
  *Changes the status of a checkbox and updates the appearance of its contact elements.
@@ -352,6 +297,7 @@ function setCheckbox(checkbox, name, i) {
     }
 }
 
+
 /**
  * shows the circle with the initials from the selected contacts.
  * 
@@ -369,6 +315,11 @@ function renderInitialsimg(i) {
 }
 
 
+/**
+ * removes the Contact initials, if checkbox is not checked for the contact
+ * 
+ * @param {number} i 
+ */
 function removeInitialsimg(i) {
     let content = document.getElementById('selected-contacts');
     let divToRemove = document.getElementById(`initials${i}`);
@@ -382,6 +333,7 @@ function removeInitialsimg(i) {
 function pushContact(name) {
     assignedContact.push(name);
 }
+
 
 /**
  * delete contact with uncheck checkbox in setCheckbox(). 
@@ -398,36 +350,11 @@ function spliceContact(name) {
 }
 
 
-function removeCheckboxStyle() {
-    let overlayContainer = document.getElementById('contact-overlay');
-    overlayContainer.innerHTML = '';
-    loadContacts();
-}
-
-
-function closeOnClickOutside(event) {
-    let overlayContainer = document.getElementById('contact-overlay');
-    let assignedTo = document.getElementById('assignedTo');
-
-    // Überprüfung ob der Klick außerhalb des Popups liegt
-    if (!overlayContainer.contains(event.target) && event.target !== assignedTo) {
-        closeContactOverlay();
-    }
-}
-
-
-function handleEnterKeyPress(event, action, i) {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        if (action === 'subtask-input') {
-            addSubTask();
-        } else if (action === 'edit-Input') {
-            renameSubTask(i);
-        }
-    }
-}
-
-
+/**
+ * This function is for the edit the Subtasks
+ * 
+ * @param {number} i 
+ */
 function renameSubTask(i) {
     let editSubTask = document.getElementById(`edit-task-input${i}`).value;
     let newSubtask = {
@@ -442,8 +369,9 @@ function renameSubTask(i) {
     addSubTask();
 }
 
+
 /**
- * only for the style for editing the subtask.
+ * only for open the for edit  subtask function.
  * 
  * @param {number} i 
  */
@@ -457,105 +385,19 @@ function editSubTask(i, currenTask) {
 }
 
 
-function styleEditSubTask(i, currenTask, subtaskList, editSubInput, taskbtn, inputContainer) {
-    inputContainer.classList.add('d-flex');
-    inputContainer.classList.remove('d-none');
-    taskbtn.classList.remove('d-flex');
-    taskbtn.classList.add('d-none');
-    subtaskList.classList.remove('subtask-list');
-    subtaskList.classList.add('d-none');
-    editSubInput.classList.remove('d-none');
-    editSubInput.value = `${currenTask}`;
-}
-
-
+/**
+ * delete the Subtasks from the current Subtasks array.
+ * 
+ * @param {number} i 
+ */
 function deleteSubTask(i) {
     newSubTasks.splice(i, 1);
     addSubTask();
-}
-
-/**
- * removes everything from the page
- */
-function clearTasks() {
-    let title = document.getElementById('title-input');
-    let description = document.getElementById('description-textarea');
-    let date = document.getElementById('date-input');
-    let sublist = document.querySelectorAll('.sublist-container');
-    let newTasks = document.getElementById('subtask-input');
-    let contactImg = document.getElementById('selected-contacts');
-
-    removeValues(title, description, date, sublist, newTasks, contactImg);
-    newSubTasks = [];
-    assignedContact = [];
-    removeCheckboxStyle();
-    removeButtonColor();
-}
-
-
-function removeValues(title, description, date, sublist, newTasks, contactImg) {
-    contactImg.innerHTML = "";
-    newTasks.value = "";
-    date.value = "";
-    description.value = "";
-    title.value = "";
-    sublist.forEach(function (element) {
-        element.innerHTML = '';
-    })
-}
-
-
-function removeButtonColor() {
-    let buttons = document.querySelectorAll('.prio-buttons button');
-    buttons.forEach(function (btn,) {
-        btn.classList.remove('active');
-    });
-    let images = document.querySelectorAll('.prio-buttons button img');
-    images.forEach(function (imag,) {
-        imag.classList.remove('active');
-    });
-}
-
-/**
- * Search function to find Contacts in the List.
- */
-function findContact() {
-    const searchInput = document.getElementById('assignedTo');
-    const contactCards = document.querySelectorAll('.add-task-contacts');
-    searchInput.addEventListener('input', () => {
-        const searchText = searchInput.value.toLowerCase();
-        contactCards.forEach((card) => {
-            const cardText = card.innerText.toLowerCase();
-            if (cardText.includes(searchText)) {
-                card.style.display = 'flex';
-            } else {
-                card.style.display = 'none';
-            }
-        });
-    });
-}
-
-
-function handleInputFocus() {
-    const inputField = document.getElementById('subtask-input');
-    const svgContainer = document.querySelector('.subtask-svg');
-    const subtaskbtn = document.getElementById('subtask-button');
-
-    inputField.addEventListener('focus', () => {
-        svgContainer.style.display = 'flex';
-        subtaskbtn.style.display = 'none';
-    });
-
-    inputField.addEventListener('blur', () => {
-        svgContainer.style.display = 'none';
-        subtaskbtn.style.display = 'flex';
-    });
 }
 
 
 function clearSubtaskInput() {
     let newTasks = document.getElementById('subtask-input');
     newTasks.value = "";
-
 }
 
